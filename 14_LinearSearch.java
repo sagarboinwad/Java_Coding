06a05eb827e3a273de8bdf120aa765bd852cbcb2
Sagar Vikas Boinwad <sagarboinwad74@gmail.com>
2025-11-19 17:12:20 +0530

    Add linear search implementation in Lsearch class
    Implemented a linear search method for an ArrayList.

--- 14_LinearSearch.java
+++ 14_LinearSearch.java
@@ -1,1 +1,28 @@
+package java_coding;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Lsearch {
+
+	public static void main(String[] args) {
+		
+		ArrayList a = new ArrayList(Arrays.asList(100,"RAM",null,12.5));
+		
+		System.out.println("Output of an Array List :"+a);
+		System.out.println(isLinearSearch(a, "RAM")==true?"found":"not found");
+		
+
+	}
+	
+	public static boolean isLinearSearch(ArrayList a,Object find) {
+		for(int i = 0; i <= a.size()-1; i++) {
+			if(a.get(i).equals(find)) {
+				return true;
+			}
+		}
+		return false;
+		
+	}
+
+}
 
