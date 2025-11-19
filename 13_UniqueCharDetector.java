ebabf3451c5823084e036fc8fb3bc4748e1ec6d8
Sagar Vikas Boinwad <sagarboinwad74@gmail.com>
2025-11-19 17:02:45 +0530

    Add UniqueCharDetector class for string analysis
    Implement a program to detect unique characters in a string using ASCII values.

--- 13_UniqueCharDetector.java
+++ 13_UniqueCharDetector.java
@@ -1,1 +1,37 @@
+package thread_practice;
+import java.util.Scanner;
+
+// unique string
+// eg- aabaadcc -> output-b
+// use ASCII value
+ 
+public class UniqueCharDetector{
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter string :");
+		String str = sc.nextLine();
+		
+		boolean found = false;
+		int[] arr = new int[256];
+		for(char c : str.toCharArray()) {
+			arr[c]++;
+		}
+		for(char c : str.toCharArray()){
+			if(arr[c] == 1) {
+				System.out.println(c);
+				found = true;
+				break;
+			}
+		}
+		if(!found) {
+			System.out.println(-1);
+		}
+	    
+		
+		
+	}
+}
 
