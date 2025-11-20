6f212cdff92669381a01dad5984bdad7c6c9ac68
Sagar Vikas Boinwad <sagarboinwad74@gmail.com>
2025-11-20 17:31:13 +0530

    Create 26_LargestString.java

---
+++ 26_LargestString.java
@@ -0,0 +1,28 @@
+package thread_practice;
+
+import java.util.Scanner;
+
+public class Demo {
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of String :");
+		String str = sc.nextLine();
+		String[] arr = str.split(" ");
+		
+		
+		String longest = "";
+
+		for(String s : arr) {
+			if(s.length() > longest.length()) {
+				longest = s;
+				
+			}
+		}
+		System.out.println("Largest String :"+longest);
+		sc.close();
+	}
+}
+
+ 
